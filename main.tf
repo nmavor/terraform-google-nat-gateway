966b751d6e0e27e3b458bb3ce5a05c1e7b8bf00e
Nadav Mavor <nmavor@vidyo.com>
2018-03-17 11:16:23 -0400

    add project to google_compute_address

--- main.tf
+++ main.tf
@@ -96,5 +96,6 @@ resource "google_compute_firewall" "nat-gateway" {
 }
 
 resource "google_compute_address" "default" {
+  project = "${var.project}"
   name = "${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
 }
